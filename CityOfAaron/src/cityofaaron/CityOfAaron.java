abf812f13ced806d1c37f3586247e493679404b6
Carol Howe <jchowe2015@gmail.com>
2018-05-16 16:37:56 -0600

    05.14.2018 City of Aaron project CIT 260

--- CityOfAaron/src/cityofaaron/CityOfAaron.java
+++ CityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * the main() class file for the cityOfAaron project
+ * CIT 260
+ * Spring 2018
+ * Team members: Tiffany Romrell, Carol Howe, Benjamin Wood
  */
 package cityofaaron;
 
