1ac48737906009379837561f66e3295cc9131d25
Carol Howe <jchowe2015@gmail.com>
2018-05-17 16:14:53 -0600

    05.14.2018 City of Aaron project CIT 260 game class

---
+++ CityOfAaron/src/byui/cit260/CityOfAaron/model/Game.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.CityOfAaron.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author carolhowe
+ */
+public class Game implements Serializable {
+    
+    // vlass intance variables
+    
+   private Player thePlayer;
+   public Game() {
+    }
+   
+
+    public Player getThePlayer() {
+        return thePlayer;
+    }
+
+    public void setThePlayer(Player thePlayer) {
+        this.thePlayer = thePlayer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.thePlayer);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "thePlayer=" + thePlayer + '}';
+    }
+    
+   
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.thePlayer, other.thePlayer)) {
+            return false;
+        }
+        return true;
+    }
+   
+   
+    
+}
