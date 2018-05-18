5979d15aacf1e4573d1585a3f6969dd84a158bc7
Tiffany <tdrdancingpianos@gmail.com>
2018-05-18 15:53:22 -0600

    Just created and added stuff to it.

---
+++ CityOfAaron/src/byui/cit260/CityOfAaron/model/ListItem.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.CityOfAaron.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author tdrda
+ */
+public class ListItem implements Serializable{
+    
+    private String name;
+    private int number;
+
+    
+    public ListItem(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + this.number;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ListItem other = (ListItem) obj;
+        if (this.number != other.number) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    
+
+    
+    
+}
