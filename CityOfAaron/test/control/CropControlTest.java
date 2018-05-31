85baf6de375f61fd7d7eda9e038aff2a1293b357
Wood-Benjamin <38670310+Wood-Benjamin@users.noreply.github.com>
2018-05-30 20:23:12 -0600

    bw - added all tests

--- CityOfAaron/test/control/CropControlTest.java
+++ CityOfAaron/test/control/CropControlTest.java
@@ -29,10 +29,68 @@ public class CropControlTest {
             theCrops.setWheatInStore(1000);
             theCrops.setAcresOwned(2800);
             int toSell = 10;
-            int price = 15;
+            int price = 20;
             int expResult = 2790;
             int result = CropControl.sellLand(price, toSell, theCrops);
             assertEquals(expResult, result);
     }
+   
+        //Test Input 2
+        @Test
+        public void testSellLand2()
+        {
+            System.out.println("sellLand2");
+            CropData theCrops = new CropData();
+            theCrops.setWheatInStore(1000);
+            theCrops.setAcresOwned(2800);
+            int toSell = (-5);
+            int price = 20;
+            int expResult = (-1);
+            int result = CropControl.sellLand(price, toSell, theCrops);
+            assertEquals(expResult, result);
+    }
+         //Test Input 3
+        @Test
+        public void testSellLand3()
+        {
+            System.out.println("sellLand3");
+            CropData theCrops = new CropData();
+            theCrops.setWheatInStore(1000);
+            theCrops.setAcresOwned(2800);
+            int toSell = 3000;
+            int price = 20;
+            int expResult = (-1);
+            int result = CropControl.sellLand(price, toSell, theCrops);
+            assertEquals(expResult, result);
+    }
+         //Test Input 4
+        @Test
+        public void testSellLand4()
+        {
+            System.out.println("sellLand4");
+            CropData theCrops = new CropData();
+            theCrops.setWheatInStore(1000);
+            theCrops.setAcresOwned(2000);
+            int toSell = 2000;
+            int price = 10;
+            int expResult = 0;
+            int result = CropControl.sellLand(price, toSell, theCrops);
+            assertEquals(expResult, result);
+    }
+ //Test Input boundary
+        @Test
+        public void testSellLandBoundary()
+        {
+            System.out.println("sellLandBoundary");
+            CropData theCrops = new CropData();
+            theCrops.setWheatInStore(1000);
+            theCrops.setAcresOwned(2800);
+            int toSell = (-5);
+            int price = 20;
+            int expResult = (-1);
+            int result = CropControl.sellLand(price, toSell, theCrops);
+            assertEquals(expResult, result);
+    }
+      
     
 }
