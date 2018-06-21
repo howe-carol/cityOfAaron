a47e7b31d15812bb22bb8bd7ca5dee8d827d66f9
tdsinger <tdrdancingpianos@gmail.com>
2018-06-20 22:33:40 -0600

    Update GameMenuView.java

--- CityOfAaron/src/view/GameMenuView.java
+++ CityOfAaron/src/view/GameMenuView.java
@@ -11,15 +11,15 @@ import java.util.Scanner;
  *
  * @author Tiffany Romrell
  */
-public class GameMenuView {
+public class GameMenuView extends MenuView {
     
         Scanner keyboard = new Scanner (System.in);
-        private String gameMenu;
+        private String menu;
         private int max;
     
 
         public GameMenuView() {
-            gameMenu = "\n" +
+            super( "\n" +
             "**********************************\n" +
             "*   What would you like to do?   *\n" + 
             "**********************************\n" +
@@ -27,73 +27,32 @@ public class GameMenuView {
             " 2 - View/Print a list\n" +
             " 3 - Move to a new location\n" +
             " 4 - Manage the Crops\n" +
-            " 5 - Return to the Main menu\n";
-            max = 5;
+            " 5 - Return to the Main menu\n",
+            5);
         }
-
-        public void displayMenuView()
-    {
-        int menuOption; 
-        do
-    {
-
-        //Display the menu
-        System.out.println(gameMenu);
-
-        //Prompt the user and get the user's input
-        menuOption = getMenuOption();
-
-        //Perform the desired action
-        doAction(menuOption);
-
-        //Determine and display the next view
-    }  
-        while (menuOption != max);
-    }
-        
-        public int getMenuOption()
-    {
-        int userInput;
-
-    // begin loop
-        do
-    {
-    // get user input from the keyboard
-        userInput = keyboard.nextInt();
-
-    // if it is not a valid value, output an error message
-        if (userInput < 1 || userInput > max)
-    {
-        System.out.println("\nOption must be between 1 and " + max);
-    }
-    // loop back to the top if input was not valid 
-        } while(userInput < 1 || userInput > max);
-    // return the value input by the user
-        return userInput;
-    }
         
         
         
-        public void doAction(int option)
-{
-    switch (option)
-    {
-        case 1: // create and start a new game
-            viewTheMap();
-            break;
-        case 2: // get and start a saved game
-            viewOrPrintAList();
-            break;
-        case 3: // get help menu
-            moveToANewLocation();
-            break;
-        case 4: // save game
-            manageTheCrops();
-            break; 
-        case 5: // return to main menu
-            break;
-    }
-}
+        @Override public void doAction(int option)
+        {
+            switch (option)
+            {
+                case 1: // create and start a new game
+                    viewTheMap();
+                    break;
+                case 2: // get and start a saved game
+                    viewOrPrintAList();
+                    break;
+                case 3: // get help menu
+                    moveToANewLocation();
+                    break;
+                case 4: // save game
+                    manageTheCrops();
+                    break; 
+                case 5: // return to main menu
+                    break;
+            }
+        }
 
         private void viewTheMap() {
             
@@ -115,4 +74,6 @@ public class GameMenuView {
         }
    
 }
+
+
 
